6bf4dff99f581fb41911bdfc7353d14e6fb6b302
ajaz-patel <ajaz.patel@stridelysolutions.com>
2024-07-10 17:16:45 +0530

    ui changes and category filter feature added successfully'

--- src/app/components/public/home/home.component.ts
+++ src/app/components/public/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { BlogpostService } from '../../features/blog-post/services/blogpost.service';
 import { Observable } from 'rxjs';
 import { BlogPost } from '../../features/blog-post/models/blogpost.model';
+import { SelectCategoryNameService } from '../../shared/services/selectCategoryName.service';
 
 @Component({
   selector: 'app-home',
@@ -9,11 +10,36 @@ import { BlogPost } from '../../features/blog-post/models/blogpost.model';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  blogs$?: Observable<BlogPost[]>;
+  blogs: BlogPost[] = []
 
-  constructor(private _blogPostService: BlogpostService) {}
+  name: string = ""
+
+  constructor(private _blogPostService: BlogpostService, private _catergorySelect: SelectCategoryNameService) {}
 
   ngOnInit() {
-    this.blogs$ = this._blogPostService.getAllBlogPosts();
+    this._catergorySelect.selectedCategory$.subscribe({
+      next: (name) => {
+        this.name = name
+        console.log(this.name);
+        this.loadBlogs()
+      }
+    });
+    this.loadBlogs();
+  }
+
+  loadBlogs() {
+    this._blogPostService.getAllBlogPosts().subscribe({
+      next: (response) => {
+        this.blogs = response;
+        if (this.name !== "") {
+          this.blogs = this.blogs.filter((blog) => {
+            return blog.categories.some((c) => c.name === this.name);
+          });
+        }
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
   }
 }
